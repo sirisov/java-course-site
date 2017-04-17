1d944226c2e1d7624360a8f451c5fe8f22e281a8
Sergey IRISOV <sergey.irisov@al-enterprise.com>
2017-04-17 16:28:51 +0300

    Reload resources if changed

--- src/main/java/com/alcatel_lucent/server_automation/java_course_server/Server.java
+++ src/main/java/com/alcatel_lucent/server_automation/java_course_server/Server.java
@@ -1,20 +1,28 @@
 package com.alcatel_lucent.server_automation.java_course_server;
 
 import static java.nio.file.Files.lines;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import static java.util.stream.Collectors.toList;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
 
+import static com.diffplug.common.base.Errors.rethrow;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import javax.util.streamex.StreamEx;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import spark.ModelAndView;
 import spark.Spark;
@@ -25,6 +33,20 @@ import com.google.gson.reflect.TypeToken;
 
 public class Server {
 
+  private static final String RES_DIR = "src/main/resources/";
+  private static final String PRESENTATIONS_FILE = "presentations.list";
+  private static final String TASKS_FILE = "tasks.json";
+  private static final Path RES_PATH = Paths.get(RES_DIR);
+  private static final Map<String, Function<Path, Object>> PATH_TO_LIST = new ConcurrentHashMap<>();
+  private static final Map<String, Object> RESOURCES = new ConcurrentHashMap<>();
+  private static WatchKey watch;
+  
+  static {
+    PATH_TO_LIST.put(PRESENTATIONS_FILE, rethrow().wrapFunction(path -> lines(path).map(Server::splitInTwo).map(Presentation::new).collect(toList())));
+    PATH_TO_LIST.put(TASKS_FILE, rethrow().wrapFunction(path -> new Gson().<List<Task>>fromJson(new FileReader(path.toFile()), new TypeToken<List<Task>>(){}.getType())
+                                                                .stream().collect(Collectors.groupingBy(Task::getGroup))));
+  }
+  
   private static <K,V> Entry<K,V> pair(K key, V value) {
     return new SimpleEntry<>(key, value);
   }
@@ -33,13 +55,26 @@ public class Server {
     return new String[] {s.substring(0, s.lastIndexOf(':')), s.substring(s.lastIndexOf(':') + 1)};
   }
   
+  private static void reloadResource(Path resPath) {
+    RESOURCES.put(resPath.toFile().getName().replaceAll("\\.[^\\.]+$", ""), PATH_TO_LIST.get(resPath.toFile().getName()).apply(resPath));
+  }
+  
+  private static Map<String, Object> getResources() throws IOException {
+    List<WatchEvent<?>> pollEvents = watch.pollEvents();
+    pollEvents.stream()
+              .filter(e -> e.kind() == ENTRY_MODIFY)
+              .map(e -> e.context()).map(Path.class::cast)
+              .map(RES_PATH::resolve)
+              .forEach(Server::reloadResource);
+    return RESOURCES;
+  }
+  
   public static void main(String[] args) throws IOException {
+    reloadResource(Paths.get(RES_DIR + PRESENTATIONS_FILE));
+    reloadResource(Paths.get(RES_DIR + TASKS_FILE));
+    watch = RES_PATH.register(FileSystems.getDefault().newWatchService(), ENTRY_MODIFY);
     port(Integer.valueOf(System.getenv().getOrDefault("PORT", "8080")));
     Spark.staticFileLocation("/public");
-    Map<String, Object> resources = StreamEx.of(
-            pair("presentations", lines(Paths.get("src/main/resources/presentations.list")).map(Server::splitInTwo).map(Presentation::new).collect(toList())),
-            pair("tasks", new Gson().fromJson(new FileReader("src/main/resources/java_base.txt"), new TypeToken<List<Task>>(){}.getType()))
-    ).toMap(Entry::getKey, Entry::getValue);
-    get("/", (req, res) -> new ModelAndView(resources, "index.ftl"), new FreeMarkerEngine());
+    get("/", (req, res) -> new ModelAndView(getResources(), "index.ftl"), new FreeMarkerEngine());
   }
 }
